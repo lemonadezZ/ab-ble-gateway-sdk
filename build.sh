ee35dea898d9200f7f3eef6ee7e72c89748fa2fc
jingzhixiang <jingzhixiang@aprbrother.com>
2017-04-21 05:22:51 +0800

    test php

--- build.sh
+++ build.sh
@@ -1,4 +1,29 @@
 #!/usr/bin/env bash
-swig -python abBleGatewaySdk.i
-gcc -c abBleGatewaySdk.c abBleGatewaySdk_wrap.c -I /usr/include/python2.7 -fPIC
-ld -shared abBleGatewaySdk.o abBleGatewaySdk_wrap.o -o _abBleGatewaySdk.so 
+SWIG=/usr/local/bin/swig
+case $1 in
+	python )
+		`$SWIG -python abBleGatewaySdk.i`
+		gcc -c abBleGatewaySdk.c abBleGatewaySdk_wrap.c -I /usr/include/python2.7 -fPIC
+		ld -shared abBleGatewaySdk.o abBleGatewaySdk_wrap.o -o _abBleGatewaySdk.so 
+	;;
+	ruby )
+		echo "TODO"
+	;;
+	php )
+		`$SWIG -php7 abBleGatewaySdk.i`
+		gcc `php-config --includes` -fpic -c abBleGatewaySdk.c abBleGatewaySdk_wrap.c 
+		ld -shared abBleGatewaySdk.o abBleGatewaySdk_wrap.o -o abBleGatewaySdk.so 
+	;;
+	golang )	
+		echo "TODO"
+	;;
+	java )
+		swig -java abBleGatewaySdk.i
+		gcc -c abBleGatewaySdk.c abBleGatewaySdk_wrap.c -I  -fPIC
+		ld -shared abBleGatewaySdk.o abBleGatewaySdk_wrap.o -o _abBleGatewaySdk.so 		
+	;;
+	*)
+		echo "$1 TODO"
+	;;
+esac
+
--- clean.sh
+++ clean.sh
@@ -1,2 +1,3 @@
-rm -rf  abBleGatewaySdk{.o,.py,.pyc,_wrap.c,_wrap.o,.so}
+rm -rf  abBleGatewaySdk{.o,.py,.pyc,_wrap.c,_wrap.o,.so,.php}
 rm -rf  _abBleGatewaySdk{.so}
+rm -rf  php_abBleGatewaySdk.h
